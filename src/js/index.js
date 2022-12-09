44c71a8b1cf34dbf18fae2050e3e7e9e002147a4
Andrey Zhigalov <zigalov.andrey@gmail.com>
2022-12-09 19:45:55 +0400

    Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,23 @@
 "use strict"
 function dropdown(event) {
     if(document.body.clientWidth < 640) {
-        let block = event.currentTarget.querySelector(".countries__block") ?? event.currentTarget.querySelector(".countries__block active")
+        let block = event.currentTarget.querySelector(".countries__block") 
         let header = event.currentTarget.querySelector("header")
+        console.log(block)
         
         if(block.getAttribute("class").includes("active")) {
-            block.setAttribute("class", "countries__block")
-            header.setAttribute("class", "")
+            block.classList.remove("active")
+            header.classList.remove("arrowUp")
         } else {
-            block.setAttribute("class", "countries__block active")
-            header.setAttribute("class", "arrowUp")
+            block.classList.add("active")
+            header.classList.add("arrowUp")
         }
     }
 }
 
 function showMenu() {
     let menu = document.querySelector(".navbar__menu") ?? document.querySelector(".navbar__menu active")
-    menu.getAttribute("class").includes("active") ? menu.setAttribute("class", "navbar__menu") : menu.setAttribute("class", "navbar__menu active")
+    menu.getAttribute("class").includes("active") ? menu.classList.remove("active") : menu.classList.add("active")
 }
 
 document.querySelectorAll(".countries__container").forEach(item => item.addEventListener("click", (event) => dropdown(event)))
