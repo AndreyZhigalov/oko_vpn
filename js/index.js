7f859a26bc4954a307a761cf1c49b7da037f2dca
Andrey Zhigalov <zigalov.andrey@gmail.com>
2022-12-10 16:31:31 +0400

    Update 2022-12-10T12:31:28.378Z

--- js/index.js
+++ js/index.js
@@ -1,25 +1,15 @@
 "use strict"
 function dropdown(event) {
-    if(document.body.clientWidth < 640) {
-        let block = event.currentTarget.querySelector(".countries__block") 
-        let header = event.currentTarget.querySelector("header")
-        console.log(block)
-        
-        if(block.getAttribute("class").includes("active")) {
-            block.classList.remove("active")
-            header.classList.remove("arrowUp")
-        } else {
-            block.classList.add("active")
-            header.classList.add("arrowUp")
-        }
+    if (document.body.clientWidth < 640) {
+        event.currentTarget.querySelector(".countries__block").classList.toggle("active")
+        event.currentTarget.querySelector("header").classList.toggle("arrowUp")
     }
 }
 
 function showMenu() {
-    let menu = document.querySelector(".navbar__menu") ?? document.querySelector(".navbar__menu active")
-    menu.getAttribute("class").includes("active") ? menu.classList.remove("active") : menu.classList.add("active")
+    document.querySelector(".navbar__menu").classList.toggle("active")
 }
 
 document.querySelectorAll(".countries__container").forEach(item => item.addEventListener("click", (event) => dropdown(event)))
 document.querySelector(".navbar__menu-button").addEventListener("click", () => showMenu())
-document.querySelectorAll(".navbar__menu").forEach(item => item.addEventListener("click", () => showMenu() ))
+document.querySelectorAll(".navbar__menu").forEach(item => item.addEventListener("click", () => showMenu()))
